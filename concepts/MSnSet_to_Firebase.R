9ce3677cc59b7b2d5afd83ae3938cdcb42e9b749
Kohze <mlw.kohze@student.ru.nl>
2017-04-20 22:48:50 +0100

    adding pRolocDatasets to Firebase

--- concepts/MSnSet_to_Firebase.R
+++ concepts/MSnSet_to_Firebase.R
@@ -23,3 +23,36 @@ retrievedData = content(data,"text")
 tempPath2 = tempfile()
 writeBin(base64_dec(fromJSON(retrievedData)), tempPath2)
 readRDS(tempPath2)
+
+# uploading all pRolocDatasets
+for(i in 1:length(a)){
+  a[i] = head(strsplit(a[i], split=" ")[[1]],1)
+}
+
+b =  a[nchar(a) > 2]
+
+#uploading all pRolocData MSnSets
+for(i in b){
+path <- paste0("/pRolocData/", i)
+data(list = i)
+tempPath = tempfile()
+saveRDS(eval(as.name(i)) , file = tempPath)
+binarySet = readBin(tempPath, what = "raw", n=500000)
+base64Set = toJSON(base64_enc(binarySet), raw = "hex")
+
+#adding content
+PUT(paste0(dbURL,path,".json"), body = base64Set)
+
+print(paste0(i, " is ready"))
+}
+
+pRolocFire <- function(dataset){
+  dbURL <- "https://firedata-b0e54.firebaseio.com"
+  path <- paste0("/pRolocData/", dataset)
+  #retrieving data
+  data = GET(paste0(dbURL,path,".json"))
+  retrievedData = content(data,"text")
+  tempPath2 = tempfile()
+  writeBin(base64_dec(fromJSON(retrievedData)), tempPath2)
+  readRDS(tempPath2)
+}
